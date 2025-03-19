db7ba862ead7f0144168b86ab4aee6705dc4fef8
Dima Paiuk <dimapajuk2325@gmail.com>
2025-03-19 22:25:32 +0200

    Enhance email validation in LoginValidator to include format check

--- UI/Components/Validators/LoginValidator.cs
+++ UI/Components/Validators/LoginValidator.cs
@@ -9,7 +9,8 @@ public class LoginValidator : AbstractValidator<LoginUserDto>
     public LoginValidator()
     {
         RuleFor(x => x.Email)
-            .NotEmpty().WithMessage("Email is required.");
+            .NotEmpty().WithMessage("Email is required.")
+            .EmailAddress().WithMessage("Invalid email format.");
         
         RuleFor(x => x.Password)
             .NotEmpty().WithMessage("Password is required.");
