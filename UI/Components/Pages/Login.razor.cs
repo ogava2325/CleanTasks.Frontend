10f5ed7dc128a45376856401f13be78f3bdc4963
Dima Paiuk <dimapajuk2325@gmail.com>
2025-03-19 22:25:16 +0200

    Refactor login process to return structured result and handle error responses

--- UI/Components/Pages/Login.razor.cs
+++ UI/Components/Pages/Login.razor.cs
@@ -13,41 +13,34 @@ public partial class Login : ComponentBase
     [Inject] private CustomAuthStateProvider AuthStateProvider { get; set; }
     [Inject] private NavigationManager NavigationManager { get; set; }
     [Inject] private IUserService UserService { get; set; }
-
-    private Validations _fluentValidation;
-
+    
     readonly LoginUserDto _loginModel = new();
 
-    private ErrorResponse? _errorResponse = new();
-    
+    private string? _errorResponse;
+
     private async Task OnLogin()
     {
-        if (await _fluentValidation.ValidateAll())
+        try
         {
-            try
+            var response = await UserService.LoginAsync(new LoginUserDto()
             {
-                var token = await UserService.LoginAsync(new LoginUserDto()
-                {
-                    Email = _loginModel.Email,
-                    Password = _loginModel.Password
-                });
+                Email = _loginModel.Email,
+                Password = _loginModel.Password
+            });
 
-                await AuthStateProvider.Login(token, _loginModel.RememberMe);
+            if (response.IsSuccess)
+            {
+                await AuthStateProvider.Login(response.Value, _loginModel.RememberMe);
                 NavigationManager.NavigateTo("/");
             }
-            catch (ApiException apiEx)
+            else
             {
-                _errorResponse = await apiEx.GetContentAsAsync<ErrorResponse>();
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine($"An error occured during login {e.Message}");
+                _errorResponse = response.Error;
             }
         }
+        catch (Exception e)
+        {
+            Console.WriteLine($"An error occured during login {e.Message}");
+        }
     }
-}
-
-public class ErrorResponse
-{
-    public string message { get; set; } = string.Empty;
 }
