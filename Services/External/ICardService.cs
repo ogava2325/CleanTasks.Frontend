088bf170edbbac0ea72797f954e7570b14005404
Dima Paiuk <dimapajuk2325@gmail.com>
2025-04-10 14:07:41 +0300

    Add authorization header to ICardService methods and update Kanban component to use token

--- Services/External/ICardService.cs
+++ Services/External/ICardService.cs
@@ -6,15 +6,24 @@ namespace services.External;
 public interface ICardService
 {
     [Get("/api/cards/{columnId}")]
-    Task<IEnumerable<CardDto>> GetByColumnId(Guid columnId);
-    
-    [Post("/api/cards")]
-    Task<Guid> CreateAsync([Body] CreateCardDto command);
-    
-    [Get("/api/cards")]
-    Task<IEnumerable<CardDto>> GetAll();
-    
-    [Put("/api/cards/{id}")]
-    Task UpdateAsync(Guid id, [Body] UpdateCardDto command);
+    Task<IEnumerable<CardDto>> GetByColumnId(
+        Guid columnId,
+        [Header("Authorization")] string authorization
+    );
 
+    [Post("/api/cards")]
+    Task<Guid> CreateAsync(
+        [Body] CreateCardDto command,
+        [Header("Authorization")] string authorization
+    );
+
+    [Get("/api/cards")]
+    Task<IEnumerable<CardDto>> GetAll([Header("Authorization")] string authorization);
+
+    [Put("/api/cards/{id}")]
+    Task UpdateAsync(
+        Guid id,
+        [Body] UpdateCardDto command,
+        [Header("Authorization")] string authorization
+    );
 }
